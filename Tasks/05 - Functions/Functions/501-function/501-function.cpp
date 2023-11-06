ae0fae05599889b811f6e6b41a701f4a264b0b51
OrionJerecca <146328725+OrionJerecca@users.noreply.github.com>
2023-11-06 13:02:20 +0000

    Trying to make a function
    Speaks for itself.

--- Tasks/05 - Functions/Functions/501-function/501-function.cpp
+++ Tasks/05 - Functions/Functions/501-function/501-function.cpp
@@ -3,29 +3,27 @@ using namespace std;
 
 int L;		//This is not an ideal location - we will soon address this
 
-int main()
+void displayBanner(){
+for (unsigned int n = 0; n < (L + 4); n++) {
+	cout << "*";
+}
+cout << endl;
+
+int main();
 {
-	const string message = "Welcome to Computer Science";
+	const string message = "Welcome to Computer Science55";
 	
 	//Get the length of the string
 	L = message.length();
 
 	//Write the banner above
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
+	displayBanner();
 
 	cout << "* ";				//Leading *
 	cout << message;			//Message
 	cout << " *" << endl;		//Trailing *
 
 	//Write the banner below
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
-
-	//Tell the calling shell all is well
-	return 0;
+	displayBanner();
+	//Tell the calling shell all is well;
 }
